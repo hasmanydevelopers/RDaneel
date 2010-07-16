60c998fd9593b67c4f8eb607185e22a3d9709a6c
Anibal Rojas <anibal@rojas.com.ve>
2010-07-16 15:06:37 -0430

    More readable URLs setup in the test

--- spec/redirect_spec.rb
+++ spec/redirect_spec.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'webrick'
 
-
 module EventMachine
   class HttpClient < Connection
     attr_accessor :redirects_count
@@ -10,12 +9,19 @@ end
 
 describe "RDaneel" do
 
-  describe "when there is no robots.txt" do
+  describe "when there is no robots.txt in the host of the uri" do
+
     before(:all) do
       start_server do |s|
-        s.mount_proc('/hello_world', lambda { |req, resp| resp.status = 200; resp.body = "hello world"})
-        s.mount_proc('/redirect', lambda { |req, resp| resp.status = 301; resp['Location'] = "http://127.0.0.1:8080/hello_world"})
-        s.mount_proc('/redirect1', lambda { |req, resp| resp.status = 301; resp['Location'] = "http://127.0.0.1:8080/redirect"})
+        s.mount_proc( '/hello_world', lambda { |req, resp|
+                      resp.status = 200
+                      resp.body = "Hello World!" } )
+        s.mount_proc( '/2nd_redirect', lambda { |req, resp|
+                      resp.status = 301
+                      resp['Location'] = "http://127.0.0.1:8080/hello_world" } )
+        s.mount_proc('/1st_redirect', lambda { |req, resp|
+                      resp.status = 301;
+                      resp['Location'] = "http://127.0.0.1:8080/2nd_redirect" } )
       end
     end
 
@@ -25,7 +31,7 @@ describe "RDaneel" do
 
     it "should work" do
       EM.run {
-        r = RDaneel.new("http://127.0.0.1:8080/redirect1")
+        r = RDaneel.new("http://127.0.0.1:8080/1st_redirect")
         r.callback {
           puts "callback"
           puts r.http_client.response
