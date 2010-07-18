aa9118f89ce6dd5e804859d3af20b0fdb8bdd8a8
Anibal Rojas <anibal@rojas.com.ve>
2010-07-18 10:49:54 -0430

    Added spec to assure that a temporary redirect (302) on robots.txt is ignored

--- spec/no_redirects_neither_robots.rb
+++ spec/no_redirects_neither_robots.rb
@@ -10,30 +10,34 @@ describe "RDaneel when there are no redirects" do
     @burrito.stop
   end
 
-  describe "when robots.txt has been moved permanently (301)" do
+  (301..302).each do |status|
 
-    it "should ignore get the content" do
+    describe "when robots.txt has been moved (#{status})" do
 
-      EM.run do
-        @burrito.mount( :path  => '/hello_world', :status => 200,
-                        :body  => 'Hello World!', :block  => should_be_hit_once )
-        @burrito.mount( :path  => '/robots.txt',  :status => 301,
-                        :location => 'http://127.0.0.1:8080/golems.txt',
-                        :block => should_be_hit_once )
-        @burrito.mount( :path  => '/golems.txt',  :status => 200,
-                        :block => should_not_be_hit )
-        r = RDaneel.new("http://127.0.0.1:8080/hello_world")
-        r.callback do
-          r.http_client.response_header.status.should == 200
-          r.http_client.response.should == "Hello World!"
-          r.redirects.should be_empty
-          EM.stop
+      it "should get the content ignoring the redirect" do
+
+        EM.run do
+          @burrito.mount( :path  => '/hello_world', :status => 200,
+                          :body  => 'Hello World!', :block  => should_be_hit_once )
+          @burrito.mount( :path  => '/robots.txt',  :status => status,
+                          :location => 'http://127.0.0.1:8080/golems.txt',
+                          :block => should_be_hit_once )
+          @burrito.mount( :path  => '/golems.txt',  :status => 200,
+                          :block => should_not_be_hit )
+          r = RDaneel.new("http://127.0.0.1:8080/hello_world")
+          r.callback do
+            r.http_client.response_header.status.should == 200
+            r.http_client.response.should == "Hello World!"
+            r.redirects.should be_empty
+            EM.stop
+          end
+          r.errback do
+            fail
+            EM.stop
+          end
+          r.get
         end
-        r.errback do
-          fail
-          EM.stop
-        end
-        r.get
+
       end
 
     end
