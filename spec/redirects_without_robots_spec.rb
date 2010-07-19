932f8fe6074562942df8069e46ca9b1290b35bcd
Anibal Rojas <anibal@rojas.com.ve>
2010-07-18 22:23:55 -0430

    Added spec to assure that when redirects are less than the maximum specified, the content is recovered following all the redirects

--- spec/redirects_without_robots_spec.rb
+++ spec/redirects_without_robots_spec.rb
@@ -45,6 +45,42 @@ describe "RDaneel when there are redirects" do
 
     describe "when a maximum number or redirects is set" do
 
+      describe "when there are less redirects than the maximum specified" do
+
+        it "should get the content following all the redirects" do
+
+          @burrito.mount( :path  => '/robots.txt',  :status => 404,
+                          :block => should_be_hit(3) )
+          @burrito.mount( :path  => '/redirect_me', :status => 301,
+                          :location  => 'http://127.0.0.1:8080/redirect_me_again',
+                          :block  => should_be_hit_once )
+          @burrito.mount( :path  => '/redirect_me_again', :status => 301,
+                          :location  => 'http://127.0.0.1:8080/hello_world',
+                          :block  => should_be_hit_once )
+          @burrito.mount( :path  => '/hello_world', :status => 200,
+                          :body  => 'Hello World!',
+                          :block  => should_be_hit_once )
+          EM.run do
+            r = RDaneel.new("http://127.0.0.1:8080/redirect_me")
+            r.callback do
+              r.http_client.response_header.status.should == 200
+              r.http_client.response.should == "Hello World!"
+              r.redirects.should == [ "http://127.0.0.1:8080/redirect_me",
+                                      "http://127.0.0.1:8080/redirect_me_again"]
+              r.uri.to_s.should == "http://127.0.0.1:8080/hello_world"
+              EM.stop
+            end
+            r.errback do
+              fail
+              EM.stop
+            end
+            r.get(:redirects => 3)
+          end
+
+        end
+
+      end
+
       describe "when there are as many redirects as the maximum" do
 
         it "should get the content following all the redirects" do
