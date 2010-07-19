965aa23ad14ca42c5f0ae9c1289686db0a061598
Anibal Rojas <anibal@rojas.com.ve>
2010-07-18 22:15:02 -0430

    Added spec to assure that when redirects matches the maximum specified, the content is recovered

--- spec/redirects_without_robots_spec.rb
+++ spec/redirects_without_robots_spec.rb
@@ -4,11 +4,11 @@ describe "RDaneel when there are redirects" do
 
   describe "when there is no robots.txt in the host (ONLY one host)" do
 
-    before(:all) do
+    before(:each) do
       @burrito = Burrito.new
     end
 
-    after(:all) do
+    after(:each) do
       @burrito.stop
     end
 
@@ -43,6 +43,41 @@ describe "RDaneel when there are redirects" do
 
     end
 
+    describe "when a maximum number or redirects is set" do
+
+      describe "when there are as many redirects as the maximum" do
+
+        it "should get the content following all the redirects" do
+
+          @burrito.mount( :path  => '/robots.txt',  :status => 404,
+                          :block => should_be_hit_twice )
+          @burrito.mount( :path  => '/redirect_me', :status => 301,
+                          :location  => 'http://127.0.0.1:8080/hello_world',
+                          :block  => should_be_hit_once )
+          @burrito.mount( :path  => '/hello_world', :status => 200,
+                          :body  => 'Hello World!',
+                          :block  => should_be_hit_once )
+          EM.run do
+            r = RDaneel.new("http://127.0.0.1:8080/redirect_me")
+            r.callback do
+              r.http_client.response_header.status.should == 200
+              r.http_client.response.should == "Hello World!"
+              r.redirects.should == ['http://127.0.0.1:8080/redirect_me']
+              r.uri.to_s.should == "http://127.0.0.1:8080/hello_world"
+              EM.stop
+            end
+            r.errback do
+              fail
+              EM.stop
+            end
+            r.get(:redirects => 1)
+          end
+
+        end
+
+      end
+
+    end
 
   end
 
