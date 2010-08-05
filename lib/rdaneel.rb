ebea6d0470c99189ac00843400e348ba35493477
Anibal Rojas <anibal@rojas.com.ve>
2010-08-05 13:29:40 -0430

    Because of weird behaviour of logger (I suppose associated to EventMachine, but not sure) switched to a simplist 'verbose mode' schema

--- lib/rdaneel.rb
+++ lib/rdaneel.rb
@@ -23,16 +23,10 @@ class RDaneel
     @uri = uri.kind_of?(Addressable::URI) ? uri : Addressable::URI::parse(uri)
     @uri.path = "/" if @uri.path.nil? || @uri.path == ""
     @redirects = []
-    @logger = options[:logger]
-    unless @logger
-      require 'logger'
-      @logger = Logger.new($stderr)
-      @logger.level = Logger::ERROR
-    end
+    @verbose = options[:verbose]
   end
 
   def get(opts = {})
-    @logger.info("Starting fetching: #{@uri}")
     current_uri = @uri
     options = DEFAULT_OPTIONS.merge(opts)
     max_redirects = options.delete(:redirects).to_i
@@ -44,24 +38,24 @@ class RDaneel
       if success?(h)
         @uri = current_uri if current_uri != @uri
         @http_client = h
-        @logger.info("Succeded fetching: #{current_uri} (#{@uri})")
+        puts("Succeded fetching: #{current_uri} (#{@uri})") if @verbose
         succeed(self)
       elsif redirected?(h)
         if @redirects.size >= max_redirects
           @http_client = h
           @error = "Exceeded maximum number of redirects (#{max_redirects})"
-          @logger.error(@error)
+          puts(@error) if @verbose
           fail(self)
           return
         end
         begin
-          @logger.info("Redirected to: #{current_uri.to_s}")
           @redirects << current_uri.to_s
           current_uri = redirect_url(h, current_uri)
+          puts("Redirected to: #{current_uri.to_s} from #{@redirects[-1]}") if @verbose
           if @redirects.include?(current_uri.to_s)
             @http_client = h
             @error = "infinite redirect"
-            @logger.error(@error)
+            puts(@error) if @verbose
             fail(self)
             return
           end
@@ -69,89 +63,87 @@ class RDaneel
         rescue
           @http_client = h
           @error = "mal formed redirected url"
-          @logger.error(@error)
+          puts(@error) if @verbose
           fail(self)
         end
       else
         # other error
         @http_client = h
         @error = "not success and not redirect"
-        @logger.error(@error)
+        puts(@error) if @verbose
         fail(self)
       end
     }
     _get = lambda {
       robots_url = robots_txt_url(current_uri)
-      @logger.info("Robots URL: #{robots_url}")
       if robots_cache && robots_file = robots_cache[robots_url.to_s]
-        @logger.info('robots.txt is cached')
+        puts("Found cached robots.txt(#{robots_url.to_s}):\n#{robots_cache[robots_url.to_s]}") if @verbose
         if robots_allowed?(robots_file, useragent, robots_url, current_uri)
-          @logger.info('Robots are allowed')
+          puts("Robots are allowed to access #{current_uri}") if @verbose
           begin
-            @logger.info("Starting fetching: #{current_uri}")
+            puts("Started fetching: #{current_uri}") if @verbose
             h = EM::HttpRequest.new(current_uri).get(options)
             h.callback(&_handle_uri_callback)
             h.errback {
               @http_client = h
               @error = h.error
-              @logger.error(@error)
+              puts(@error) if @verbose
               fail(self)
             }
           rescue StandardError => se
             @http_client = EM::HttpClient.new("")
             @error = "#{se.message}\n#{se.backtrace.inspect}"
-            @logger.error(@error)
+            puts(@error) if @verbose
             fail(self)
           end
         else
           @http_client = EM::HttpClient.new("")
           @error = "robots denied"
-          @logger.error(@error)
+          puts(@error) if @verbose
           fail(self)
         end
       else
         robots_url = robots_txt_url(current_uri)
-        @logger.info("Starting fetching robots.txt at: #{robots_url}")
+        puts("Started fetching robots.txt from: #{robots_url} for #{current_uri}") if @verbose
         robots = EM::HttpRequest.new(robots_url).get
         robots.callback {
-          @logger.info("Found robots.txt at: #{current_uri}")
           robots_file = robots.response
-          @logger.info(robots_file)
+          puts("Found robots.txt at #{current_uri}:\n#{robots_file}") if @verbose
           robots_cache[robots_url.to_s] = robots_file if robots_cache
           if robots_allowed?(robots_file, useragent, robots_url, current_uri)
-            @logger.info('Robots are allowed')
+            puts("Robots are allowed to access #{current_uri}") if @verbose
             begin
-              @logger.info("Starting fetching: #{current_uri}")
+              puts("Started fetching: #{current_uri}") if @verbose
               h = EM::HttpRequest.new(current_uri).get(options)
               h.callback(&_handle_uri_callback)
               h.errback {
                 @http_client = h
                 @error = h.error
-                @logger.error(@error)
+                puts(@error) if @verbose
                 fail(self)
               }
             rescue StandardError => se
               @http_client = EM::HttpClient.new("")
               @error = "#{se.message}\n#{se.backtrace.inspect}"
-              @logger.error(@error)
+              puts(@error) if @verbose
               fail(self)
             end
           else
             @http_client = EM::HttpClient.new("")
             @error = "robots denied"
-            @logger.error(@error)
+            puts(@error) if @verbose
             fail(self)
           end
         }
         robots.errback {
           robots_cache[robots_url.to_s] = "" if robots_cache
-          @logger.info("Starting fetching: #{current_uri}")
+          puts("Started fetching: #{current_uri}") if @verbose
           h = EM::HttpRequest.new(current_uri).get(options)
           h.callback(&_handle_uri_callback)
           h.errback {
             @http_client = h
             @error = h.error
-            @logger.error(@error)
+            puts(@error) if @verbose
             fail(self)
           }
         }
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -3,50 +3,12 @@ require 'logger'
 
 describe "RDaneel" do
 
-  describe "when there is not logger option set" do
-
-    before(:each) do
-      RDaneel.robots_cache = {}
-      burrito.mount( :path  => '/robots.txt',  :status => 404 )
-      burrito.mount( :path  => '/redirect_me', :status => 301,
-                     :location  => 'http://127.0.0.1:8080/hello_world' )
-      burrito.mount( :path  => '/hello_world', :status => 200,
-                     :body  => 'Hello World!' )
-    end
-
-    after(:each) do
-      burrito.unmount('/robots.txt')
-      burrito.unmount('/redirect_me')
-      burrito.unmount('/hello_world')
-    end
-
-    it "should not log anything to standard error" do
-      $stderr = StringIO.new
-      EM.run do
-        r = RDaneel.new("http://127.0.0.1:8080/redirect_me")
-        r.callback do
-          r.http_client.response_header.status.should == 200
-          r.http_client.response.should == "Hello World!"
-          r.redirects.should == [ "http://127.0.0.1:8080/redirect_me"]
-          r.uri.to_s.should == "http://127.0.0.1:8080/hello_world"
-          EM.stop
-        end
-        r.errback do
-          fail
-          EM.stop
-        end
-        r.get(:redirects => 3)
-        $stderr.read.should be_empty
-      end
-    end
-
-  end
-
   describe "when there is a logger option set" do
 
     before(:each) do
       RDaneel.robots_cache = {}
-      burrito.mount( :path  => '/robots.txt',  :status => 404 )
+      burrito.mount( :path  => '/robots.txt', :status => 200,
+                     :body  => "User-agent: *\nDisallow: /private" )
       burrito.mount( :path  => '/redirect_me', :status => 301,
                      :location  => 'http://127.0.0.1:8080/hello_world' )
       burrito.mount( :path  => '/hello_world', :status => 200,
@@ -60,11 +22,9 @@ describe "RDaneel" do
     end
 
     it "should log to standard error" do
-      sio = StringIO.new
-      siolog = Logger.new(sio)
-      siolog.level = Logger::INFO
+
       EM.run do
-        r = RDaneel.new("http://127.0.0.1:8080/redirect_me", :logger => siolog)
+        r = RDaneel.new("http://127.0.0.1:8080/redirect_me", :verbose => true)
         r.callback do
           r.http_client.response_header.status.should == 200
           r.http_client.response.should == "Hello World!"
@@ -77,10 +37,6 @@ describe "RDaneel" do
           EM.stop
         end
         r.get(:redirects => 3)
-        sio.rewind
-        sio.read.should_not be_empty
-        sio.rewind
-        sio.each_line { |l| puts l }
       end
     end
 
