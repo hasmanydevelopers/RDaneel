e5f0339162c3cc1c67f21680ed8ac9263863ace1
Anibal Rojas <anibal@rojas.com.ve>
2010-08-05 12:42:09 -0430

    Umm, not sure what is happening, look like log is being truncated

--- lib/rdaneel.rb
+++ lib/rdaneel.rb
@@ -26,12 +26,13 @@ class RDaneel
     @logger = options[:logger]
     unless @logger
       require 'logger'
-      @logger = Logger.new(STDERR)
-      @logger.level = Logger::FATAL
+      @logger = Logger.new($stderr)
+      @logger.level = Logger::ERROR
     end
   end
 
   def get(opts = {})
+    @logger.info("Starting fetching: #{@uri}")
     current_uri = @uri
     options = DEFAULT_OPTIONS.merge(opts)
     max_redirects = options.delete(:redirects).to_i
@@ -43,12 +44,13 @@ class RDaneel
       if success?(h)
         @uri = current_uri if current_uri != @uri
         @http_client = h
+        @logger.info("Succeded fetching: #{current_uri} (#{@uri})")
         succeed(self)
       elsif redirected?(h)
         if @redirects.size >= max_redirects
           @http_client = h
-          @error = "Exceeded maximum number of redirects"
-          @logger.info(@error)
+          @error = "Exceeded maximum number of redirects (#{max_redirects})"
+          @logger.error(@error)
           fail(self)
           return
         end
@@ -59,7 +61,7 @@ class RDaneel
           if @redirects.include?(current_uri.to_s)
             @http_client = h
             @error = "infinite redirect"
-            @logger.info(@error)
+            @logger.error(@error)
             fail(self)
             return
           end
@@ -67,72 +69,89 @@ class RDaneel
         rescue
           @http_client = h
           @error = "mal formed redirected url"
-          @logger.info(@error)
+          @logger.error(@error)
           fail(self)
         end
       else
         # other error
         @http_client = h
         @error = "not success and not redirect"
-        @logger.info(@error)
+        @logger.error(@error)
         fail(self)
       end
     }
     _get = lambda {
       robots_url = robots_txt_url(current_uri)
+      @logger.info("Robots URL: #{robots_url}")
       if robots_cache && robots_file = robots_cache[robots_url.to_s]
+        @logger.info('robots.txt is cached')
         if robots_allowed?(robots_file, useragent, robots_url, current_uri)
+          @logger.info('Robots are allowed')
           begin
+            @logger.info("Starting fetching: #{current_uri}")
             h = EM::HttpRequest.new(current_uri).get(options)
             h.callback(&_handle_uri_callback)
             h.errback {
               @http_client = h
               @error = h.error
+              @logger.error(@error)
               fail(self)
             }
           rescue StandardError => se
             @http_client = EM::HttpClient.new("")
             @error = "#{se.message}\n#{se.backtrace.inspect}"
+            @logger.error(@error)
             fail(self)
           end
         else
           @http_client = EM::HttpClient.new("")
           @error = "robots denied"
+          @logger.error(@error)
           fail(self)
         end
       else
         robots_url = robots_txt_url(current_uri)
+        @logger.info("Starting fetching robots.txt at: #{robots_url}")
         robots = EM::HttpRequest.new(robots_url).get
         robots.callback {
+          @logger.info("Found robots.txt at: #{current_uri}")
           robots_file = robots.response
+          @logger.info(robots_file)
           robots_cache[robots_url.to_s] = robots_file if robots_cache
           if robots_allowed?(robots_file, useragent, robots_url, current_uri)
+            @logger.info('Robots are allowed')
             begin
+              @logger.info("Starting fetching: #{current_uri}")
               h = EM::HttpRequest.new(current_uri).get(options)
               h.callback(&_handle_uri_callback)
               h.errback {
                 @http_client = h
                 @error = h.error
+                @logger.error(@error)
                 fail(self)
               }
             rescue StandardError => se
               @http_client = EM::HttpClient.new("")
               @error = "#{se.message}\n#{se.backtrace.inspect}"
+              @logger.error(@error)
               fail(self)
             end
           else
             @http_client = EM::HttpClient.new("")
             @error = "robots denied"
+            @logger.error(@error)
             fail(self)
           end
         }
         robots.errback {
           robots_cache[robots_url.to_s] = "" if robots_cache
+          @logger.info("Starting fetching: #{current_uri}")
           h = EM::HttpRequest.new(current_uri).get(options)
           h.callback(&_handle_uri_callback)
           h.errback {
             @http_client = h
             @error = h.error
+            @logger.error(@error)
             fail(self)
           }
         }
