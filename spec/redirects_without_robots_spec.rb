e3c839a433598fed03b20dcaa51207e5240d1bcd
Edgar <edgargonzalez@gmail.com>
2010-08-13 13:56:52 -0430

    Adjusted the spec, now start a webrick instance for spec

--- spec/redirects_without_robots_spec.rb
+++ spec/redirects_without_robots_spec.rb
@@ -2,29 +2,31 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "RDaneel when there are redirects" do
 
+  let(:port) {8081}
+
   describe "when there is no robots.txt in the host (ONLY one host)" do
 
     describe "when no redirection limit has been set" do
       before(:each) do
-        burrito.mount( :path  => '/robots.txt',  :status => 404,
+        server_setup(port) do |server|
+          mount(server, :path  => '/robots.txt',  :status => 404,
                         :block => should_be_hit_once )
-        burrito.mount( :path  => '/redirect_me', :status => 301,
-                        :location  => 'http://127.0.0.1:8080/hello_world',
+          mount(server, :path  => '/redirect_me', :status => 301,
+                        :location  => "http://127.0.0.1:#{port}/hello_world",
                         :block  => should_be_hit_once )
-        burrito.mount( :path  => '/hello_world', :status => 200,
+          mount(server, :path  => '/hello_world', :status => 200,
                         :body  => 'Hello World!',
                         :block  => should_not_be_hit )
+        end
       end
 
       after(:each) do
-        burrito.unmount('/robots.txt')
-        burrito.unmount('/redirect_me')
-        burrito.unmount('/hello_world')
+        server_shutdown
       end
 
       it "should not follow redirects" do
         EM.run do
-          r = RDaneel.new("http://127.0.0.1:8080/redirect_me")
+          r = RDaneel.new("http://127.0.0.1:#{port}/redirect_me")
           r.callback do
             fail
             EM.stop
@@ -45,35 +47,34 @@ describe "RDaneel when there are redirects" do
 
       describe "when there are less redirects than the maximum specified" do
         before(:each) do
-          burrito.mount( :path  => '/robots.txt',  :status => 404,
+          server_setup(port) do |server|
+            mount(server, :path  => '/robots.txt',  :status => 404,
                           :block => should_be_hit(3) )
-          burrito.mount( :path  => '/redirect_me', :status => 301,
-                          :location  => 'http://127.0.0.1:8080/redirect_me_again',
+            mount(server, :path  => '/redirect_me', :status => 301,
+                          :location  => "http://127.0.0.1:#{port}/redirect_me_again",
                           :block  => should_be_hit_once )
-          burrito.mount( :path  => '/redirect_me_again', :status => 301,
-                          :location  => 'http://127.0.0.1:8080/hello_world',
+            mount(server, :path  => '/redirect_me_again', :status => 301,
+                          :location  => "http://127.0.0.1:#{port}/hello_world",
                           :block  => should_be_hit_once )
-          burrito.mount( :path  => '/hello_world', :status => 200,
+            mount(server, :path  => '/hello_world', :status => 200,
                           :body  => 'Hello World!',
                           :block  => should_be_hit_once )
+          end
         end
 
         after(:each) do
-          burrito.unmount('/robots.txt')
-          burrito.unmount('/redirect_me')
-          burrito.unmount('/redirect_me_again')
-          burrito.unmount('/hello_world')
+          server_shutdown
         end
 
         it "should get the content following all the redirects" do
           EM.run do
-            r = RDaneel.new("http://127.0.0.1:8080/redirect_me")
+            r = RDaneel.new("http://127.0.0.1:#{port}/redirect_me")
             r.callback do
               r.http_client.response_header.status.should == 200
               r.http_client.response.should == "Hello World!"
-              r.redirects.should == [ "http://127.0.0.1:8080/redirect_me",
-                                      "http://127.0.0.1:8080/redirect_me_again"]
-              r.uri.to_s.should == "http://127.0.0.1:8080/hello_world"
+              r.redirects.should == [ "http://127.0.0.1:#{port}/redirect_me",
+                                      "http://127.0.0.1:#{port}/redirect_me_again"]
+              r.uri.to_s.should == "http://127.0.0.1:#{port}/hello_world"
               EM.stop
             end
             r.errback do
@@ -89,30 +90,30 @@ describe "RDaneel when there are redirects" do
 
       describe "when there are as many redirects as the maximum" do
         before(:each) do
-          burrito.mount( :path  => '/robots.txt',  :status => 404,
+          server_setup(port) do |server|
+            mount(server, :path  => '/robots.txt',  :status => 404,
                           :block => should_be_hit_twice )
-          burrito.mount( :path  => '/redirect_me', :status => 301,
-                          :location  => 'http://127.0.0.1:8080/hello_world',
+            mount(server, :path  => '/redirect_me', :status => 301,
+                          :location  => "http://127.0.0.1:#{port}/hello_world",
                           :block  => should_be_hit_once )
-          burrito.mount( :path  => '/hello_world', :status => 200,
+            mount(server, :path  => '/hello_world', :status => 200,
                           :body  => 'Hello World!',
                           :block  => should_be_hit_once )
+          end
         end
 
         after(:each) do
-          burrito.unmount('/robots.txt')
-          burrito.unmount('/redirect_me')
-          burrito.unmount('/hello_world')
+          server_shutdown
         end
 
         it "should get the content following all the redirects" do
           EM.run do
-            r = RDaneel.new("http://127.0.0.1:8080/redirect_me")
+            r = RDaneel.new("http://127.0.0.1:#{port}/redirect_me")
             r.callback do
               r.http_client.response_header.status.should == 200
               r.http_client.response.should == "Hello World!"
-              r.redirects.should == ['http://127.0.0.1:8080/redirect_me']
-              r.uri.to_s.should == "http://127.0.0.1:8080/hello_world"
+              r.redirects.should == ["http://127.0.0.1:#{port}/redirect_me"]
+              r.uri.to_s.should == "http://127.0.0.1:#{port}/hello_world"
               EM.stop
             end
             r.errback do
@@ -128,35 +129,34 @@ describe "RDaneel when there are redirects" do
 
       describe "when the number of redirects exceed the maximum specified" do
         before(:each) do
-          burrito.mount( :path  => '/robots.txt',  :status => 404,
+          server_setup(port) do |server|
+            mount(server, :path  => '/robots.txt',  :status => 404,
                           :block => should_be_hit_twice )
-          burrito.mount( :path  => '/redirect_me', :status => 301,
-                          :location  => 'http://127.0.0.1:8080/redirect_me_again',
+            mount(server, :path  => '/redirect_me', :status => 301,
+                          :location  => "http://127.0.0.1:#{port}/redirect_me_again",
                           :block  => should_be_hit_once )
-          burrito.mount( :path  => '/redirect_me_again', :status => 301,
-                          :location  => 'http://127.0.0.1:8080/hello_world',
+            mount(server, :path  => '/redirect_me_again', :status => 301,
+                          :location  => "http://127.0.0.1:#{port}/hello_world",
                           :block  => should_be_hit_once )
-          burrito.mount( :path  => '/hello_world', :status => 200,
+            mount(server, :path  => '/hello_world', :status => 200,
                           :body  => 'Hello World!',
                           :block  => should_not_be_hit )
+          end
         end
 
         after(:each) do
-          burrito.unmount('/robots.txt')
-          burrito.unmount('/redirect_me')
-          burrito.unmount('/redirect_me_again')
-          burrito.unmount('/hello_world')
+          server_shutdown
         end
 
         it "should stop following redirects once the  maximum specified is reached" do
           EM.run do
-            r = RDaneel.new("http://127.0.0.1:8080/redirect_me")
+            r = RDaneel.new("http://127.0.0.1:#{port}/redirect_me")
             r.callback do
               fail
               EM.stop
             end
             r.errback do
-              r.redirects.should == ['http://127.0.0.1:8080/redirect_me']
+              r.redirects.should == ["http://127.0.0.1:#{port}/redirect_me"]
               r.error.should == "Exceeded maximum number of redirects"
               EM.stop
             end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,57 +5,66 @@ require 'rdaneel'
 require 'spec'
 require 'webrick'
 
-Spec::Runner.configure do |config|
-  config.before :suite do
-    burrito
+# keep webrick quiet
+class ::WEBrick::HTTPServer
+  def access_log(config, req, res)
+    # nop
   end
-  config.after :suite do
-    burrito.stop
+end
+class ::WEBrick::BasicLog
+  def log(level, data)
+    # nop
   end
 end
 
-def burrito
-  Thread.current[:burrito] ||= Burrito.new
+def locked_file
+  File.join(File.dirname(__FILE__),"server_lock-#{@__port}")
 end
 
-class Burrito
+def server_setup(port=8080, &blk)
+  @__port = port
+  if @server.nil? and !File.exist?(locked_file)
+    File.open(locked_file,'w') {|f| f << 'locked' }
+    @server = WEBrick::HTTPServer.new :Port => port
+    blk.call(@server) if blk
+    queue = Queue.new # synchronize the thread startup to the main thread
+    @test_thread = Thread.new { queue << 1; @server.start }
 
-  def initialize( options={}, &blk )
-    webrick_log_file = '/dev/null'  # disable logging
-    webrick_logger = WEBrick::Log.new(webrick_log_file, WEBrick::Log::DEBUG)
-    access_log_stream = webrick_logger
-    access_log = [[ access_log_stream, WEBrick::AccessLog::COMBINED_LOG_FORMAT ]]
-    default_opts = {:Port => 8080, :Logger => webrick_logger, :AccessLog => access_log }
-    @server = WEBrick::HTTPServer.new( default_opts.merge(options) )
-    @server_thread = Thread.new {
-      blk.call(@server) if blk
-      @server.start
-    }
-    @server
-    sleep 0.5
+    # wait for the queue
+    value = queue.pop
+
+    if !value
+      STDERR.puts "Failed to startup test server!"
+      exit(1)
+    end
+
+    trap("INT"){server_shutdown}
+    at_exit{server_shutdown}
   end
+end
 
-  def mount( opts )
-    raise ":path is required" if opts[:path].nil?
-    raise ":status is required" if opts[:status].nil?
-    @server.mount_proc( opts[:path],
-      lambda { |req, resp|
-               resp.status = opts[:status]
-               resp.body = opts[:body] unless opts[:body].nil?
-               resp['Location'] = opts[:location] unless opts[:location].nil?
-               opts[:block].call unless opts[:block].nil?
-             } )
+def server_shutdown
+  begin
+    if File.exist?(locked_file)
+      File.unlink locked_file
+      @server.shutdown unless @server.nil?
+      @server = nil
+    end
+  rescue Object => e
+    puts "Error #{__FILE__}:#{__LINE__}\n#{e.message}"
   end
+end
 
-  def stop
-    @server.shutdown
-    @server_thread.join
-  end
-
-  def unmount(path)
-    @server.unmount(path)
-  end
-
+def mount(server, opts)
+  raise ":path is required" if opts[:path].nil?
+  raise ":status is required" if opts[:status].nil?
+  server.mount_proc( opts[:path],
+    lambda { |req, resp|
+             resp.status = opts[:status]
+             resp.body = opts[:body] unless opts[:body].nil?
+             resp['Location'] = opts[:location] unless opts[:location].nil?
+             opts[:block].call unless opts[:block].nil?
+           } )
 end
 
 def should_not_be_hit
@@ -74,5 +83,8 @@ def should_be_hit( times = 1 )
   l = lambda {}
   m = l.should_receive(:call).exactly(times).times
   return l
+end
+
+Spec::Runner.configure do |config|
 end
 
