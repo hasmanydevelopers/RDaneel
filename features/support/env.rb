250599455bb734b7a762c605d839e784320a363a
Edgar <edgargonzalez@gmail.com>
2010-08-25 11:09:37 -0430

    Moved burrito class from spec/support to features/support

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,9 +2,9 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../..', 'lib'))
 require 'rubygems'
 require 'rdaneel'
+require 'burrito'
 
 
-Dir[File.dirname(__FILE__) + "/../../spec/support/**/*.rb"].each {|f| require f}
 
 unless $server
   $server = Burrito.new
