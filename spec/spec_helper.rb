5a51bbae5457d091981c55d8451b3cd0e514bc72
Edgar <edgargonzalez@gmail.com>
2010-08-27 17:02:28 -0430

    Removed unused specs, now are tested via features

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,4 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
 require 'rdaneel'
 require 'spec'
-Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
-
-unless $server
-  $server = Burrito.new
-  $server.start
-end
 
